2d6136f1ffcac4aa64d2474be322ec32d56901cf
ericwenn <ericwenn@gmail.com>
2015-08-27 09:59:02 +0200

    Multiple playerhandling

--- Player.py
+++ Player.py
@@ -6,16 +6,18 @@ class Player:
     def __init__(self, player):
         print(player)
 
-        if player == 'first':
+        if player == 1:
             self.left = pygame.K_a
             self.right = pygame.K_d
             self.up = pygame.K_w
             self.down = pygame.K_s
-        elif player == 'second':
+            self.shoot = pygame.K_SPACE
+        elif player == 2:
             self.left = pygame.K_LEFT
             self.right = pygame.K_RIGHT
             self.up = pygame.K_UP
             self.down = pygame.K_DOWN
+            self.shoot = pygame.K_KP_ENTER
 
         self.dirX = False
         self.dirY = False
@@ -30,9 +32,6 @@ class Player:
 
 
     def keydown(self, key):
-
-
-            print(key)
             if key == self.left:
                 self.speed[0] = -self.baseSpeed
                 self.dirX = 'left'
@@ -46,12 +45,19 @@ class Player:
             elif key == self.up:
                 self.speed[1] = -self.baseSpeed
                 self.dirY = 'up'
+            elif key == self.shoot:
+                print("SHOT")
+
+
 
     def keyup(self, key):
         if key == self.left or key == self.right:
             self.speed[0] = 0
         elif key == self.up or key == self.down:
             self.speed[1] = 0
+
+
+
 
 
     def move(self, screen):
